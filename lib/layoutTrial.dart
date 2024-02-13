9644b5212f45a8d703a1e31ca807d0785ddf3401
Lyle Patterson <lyle.patterson2002@gmail.com>
2024-02-13 17:49:11 +0000

    added lake image

--- lib/layoutTrial.dart
+++ lib/layoutTrial.dart
@@ -12,7 +12,25 @@ class MyApp2 extends StatelessWidget{
           title: const Text("Trial"),
         ),
         body: const Center(
-          child: TitleSection(location: "Edge Hill", name: "Lyle"),
+          child: Column(
+            children:[
+              ImageSection(
+                image: "images/lake.jpg",
+              ),
+              TitleSection(
+                name: 'Oeschinen Lake Campground',
+                location: 'Kandersteg, Switzerland',
+              ),
+              ButtonSection(),
+              TextSection(description: 'Lake Oeschinen lies at the foot of the Blüemlisalp in the '            
+                    +                        'Bernese Alps. Situated 1,578 meters above sea level, it '            
+                    +                        'is one of the larger Alpine Lakes. A gondola ride from '            
+                    +                        'Kandersteg, followed by a half-hour walk through pastures '            
+                    +                        'and pine forest, leads you to the lake, which warms to 20 '            
+                    +                        'degrees Celsius in the summer. Activities enjoyed here '            
+                    +                        'include rowing, and riding the summer toboggan run.',),
+            ]
+          )
         )
       )
     );
@@ -67,6 +85,109 @@ class TitleSection extends StatelessWidget{
           const Text('41'),
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget{
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children:[
+          ButtonWithText(
+            color: color,
+            icon: Icons.call,
+            label: "CALL",
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.near_me,
+            label: "ROUTE",
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.share,
+            label: "SHARE",
+          ),
+        ]
+      )
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget{
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children:[
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+              color: color,
+            )
+          )
+        )
+      ]
+    );
+  }
+}
+
+class TextSection extends StatelessWidget{
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
+      )
+    );
+
+  }
+}
+
+class ImageSection extends StatelessWidget{
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context){
+    return Image.asset(
+      image,
+      width: 612,
+      height: 404,
+      fit: BoxFit.cover,
     );
   }
 }
