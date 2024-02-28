115460d5dac485e433d5b53e7dd8d4fd88d78e77
Lyle Patterson <lyle.patterson2002@gmail.com>
2024-02-28 01:36:20 +0000

    dfxgszdrfg

--- lib/view_model/home/locationViewModel.dart
+++ lib/view_model/home/locationViewModel.dart
@@ -6,10 +6,11 @@ class LocationViewModel{
   List<Location> locations = [];
 
   Future<void> loadLocations() async{
-    final String jsonContent = await rootBundle.loadString('images/locations.json');
+    final String jsonContent = await rootBundle.loadString('jsonData/locations.json');
     final List<dynamic> jsonList = json.decode(jsonContent);
 
     locations = jsonList.map((json) => Location.fromJson(json)).toList();
 
   }
+
 }
